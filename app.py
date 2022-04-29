f9071841f1f9cb5f0d9e0a8800a11ee673c2ba40
林靖軒 <m70510108@gmail.com>
2022-04-29 12:31:49 +0800

    app good

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def about():
 app.add_url_rule('/about', 'about', about)#更路由、路由名稱、function name
     
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000, debug=True, use_reloader = True)
+    app.run(host='127.0.0.1', port=5000, debug=False, use_reloader = True)
     #debug mode=False 才不會洩漏code中的資料
     #但他不會自動變更望葉，要跳出諸端雞重啟
     #use_reloader = True自動變更
